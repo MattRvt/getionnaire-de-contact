a223c3fa7cf200fd42c6bbac57d50951c1227a62
Matthis RIVAT <matthisrivat@Gmail.com>
2022-08-31 17:14:52 +0200

    add API

---
+++ api/entreprise.php
@@ -0,0 +1,108 @@
+<?php
+// Se connecter à la base de données
+include("db_connect.php");
+$request_method = $_SERVER["REQUEST_METHOD"];
+
+switch ($request_method) {
+    case 'GET':
+        if (!empty($_GET["id"])) {
+            // Récupérer un seul produit
+            $id = intval($_GET["id"]);
+            getProducts($id);
+        } else {
+            // Récupérer tous les produits
+            getProducts();
+        }
+        break;
+    case 'POST':
+        // Ajouter un produit
+        AddProduct();
+        break;
+    default:
+        // Requête invalide
+        header("HTTP/1.0 405 Method Not Allowed");
+        break;
+}
+
+function getProducts()
+{
+    global $conn;
+    $query = "SELECT * FROM produit";
+    $response = array();
+    $result = mysqli_query($conn, $query);
+    while ($row = mysqli_fetch_array($result)) {
+        $response[] = $row;
+    }
+    header('Content-Type: application/json');
+    echo json_encode($response, JSON_PRETTY_PRINT);
+}
+
+function getProduct($id = 0)
+{
+    global $conn;
+    $query = "SELECT * FROM produit";
+    if ($id != 0) {
+        $query .= " WHERE id=" . $id . " LIMIT 1";
+    }
+    $response = array();
+    $result = mysqli_query($conn, $query);
+    while ($row = mysqli_fetch_array($result)) {
+        $response[] = $row;
+    }
+    header('Content-Type: application/json');
+    echo json_encode($response, JSON_PRETTY_PRINT);
+}
+
+function AddProduct()
+{
+    global $conn;
+    $name = $_POST["name"];
+    $description = $_POST["description"];
+    $price = $_POST["price"];
+    $category = $_POST["category"];
+    $created = date('Y-m-d H:i:s');
+    $modified = date('Y-m-d H:i:s');
+    echo $query = "INSERT INTO produit(name, description, price, category_id, created, modified) VALUES('" . $name . "', '" . $description . "', '" . $price . "', '" . $category . "', '" . $created . "', '" . $modified . "')";
+    if (mysqli_query($conn, $query)) {
+        $response = array(
+            'status' => 1,
+            'status_message' => 'Produit ajoute avec succes.'
+        );
+    } else {
+        $response = array(
+            'status' => 0,
+            'status_message' => 'ERREUR!.' . mysqli_error($conn)
+        );
+    }
+    header('Content-Type: application/json');
+    echo json_encode($response);
+}
+
+function updateProduct($id)
+{
+    global $conn;
+    $_PUT = array(); //tableau qui va contenir les données reçues
+    parse_str(file_get_contents('php://input'), $_PUT);
+    $name = $_PUT["name"];
+    $description = $_PUT["description"];
+    $price = $_PUT["price"];
+    $category = $_PUT["category"];
+    $modified = date('Y-m-d H:i:s');
+    //construire la requête SQL
+    $query = "UPDATE produit SET name='" . $name . "', description='" . $description . "', price='" . $price . "', category_id='" . $category . "', modified='" . $modified . "' WHERE id=" . $id;
+
+    if (mysqli_query($conn, $query)) {
+        $response = array(
+            'status' => 1,
+            'status_message' => 'Produit mis a jour avec succes.'
+        );
+    } else {
+        $response = array(
+            'status' => 0,
+            'status_message' => 'Echec de la mise a jour de produit. ' . mysqli_error($conn)
+        );
+    }
+
+    header('Content-Type: application/json');
+    echo json_encode($response);
+}
